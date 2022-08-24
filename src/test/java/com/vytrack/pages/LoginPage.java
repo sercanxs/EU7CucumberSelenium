733c00f2277b55613a96a1134a102e5faf9f963c
Baldwin <sercanxssahin@hotmail.com>
2022-08-24 15:31:10 +0300

    1

--- src/test/java/com/vytrack/pages/LoginPage.java
+++ src/test/java/com/vytrack/pages/LoginPage.java
@@ -7,16 +7,15 @@ import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
 
-    public LoginPage(){
+    public LoginPage() {
         PageFactory.initElements(Driver.get(), this);
     }
 
-    @FindBy(id="prependedInput")
+    @FindBy(id = "prependedInput")
     public WebElement userName;
 
 
-
-    @FindBy(id="prependedInput2")
+    @FindBy(id = "prependedInput2")
     public WebElement password;
 
     @FindBy(name = "_submit")
